a43cdd3d75b9df49f3374270a2fe1c5f8ebf1fc3
eslia0 <limento22@gmail.com>
2019-04-06 16:57:26 +0900

    remove cache

---
+++ Assets/Script/PYJ/Tutorial.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+
+public class Tutorial : MonoBehaviour
+{
+    public Text text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        string path = Application.persistentDataPath;
+
+        if (!File.Exists(path + "/data01.bin"))
+        {
+            File.WriteAllBytes(path + "/data01.bin", new byte[1]);
+            text.text = "튜토리얼";
+        }
+        else
+        {
+            text.text = "튜토 스킵";
+        }
+    }
+}
